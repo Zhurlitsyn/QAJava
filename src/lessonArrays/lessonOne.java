660aa1c767cd071216e32b7db98042e5ea915287
Zhurlitsyn <6131755@gmail.com>
2023-01-11 10:27:54 +0300

    Lesson 1: no need to control

--- src/lessonArrays/lessonOne.java
+++ src/lessonArrays/lessonOne.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class lessonOne {
         public static void main(String[] args) {
-            int arr [][] = new int[5][5];
+            int[][] arr = new int[5][5];
             int [] arrSecond = new int[5];
             ArrayRandom.fillRandom(arr);
             ArrayRandom.print2DArray(arr);
